616edaba29011d576fcb78b9cc261501b4386e80
Owen Lennon <owenlennon3@gmail.com>
2018-07-02 02:40:44 +0100

    Wrong Keys

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 #Not default as of this flask version
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-SECRET_KEY = 'hsidsid2132khs123n23n2n32mn3'
+SECRET_KEY = 'your Sercret key'
 
-SENDGRID_API_KEY='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'
+SENDGRID_API_KEY='Your API KEY'
 
 #Database
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:student33@localhost/Portal'
+SQLALCHEMY_DATABASE_URI = 'Your database'
